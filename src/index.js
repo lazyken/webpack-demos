af1837f6fb20bbca0492d1f3bfaf2388c2b2219f
zhangxiaoyu <wykenken@163.com>
2020-12-15 13:46:17 +0800

    output management complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import _ from 'lodash';
-import './index.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们现有的 div。
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
